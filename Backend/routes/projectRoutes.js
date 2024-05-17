5fc5beff2ac75cb8a4a6d7f286c759172a1f5d2b
abshir112 <abshir_muhumed.abdi0018@stud.hkr.se>
2024-05-18 00:26:59 +0200

    Add project assignment routes

---
+++ Backend/routes/projectRoutes.js
@@ -0,0 +1,9 @@
+// backend/routes/projectRoutes.js
+import express from 'express';
+import { createProject, getAllProjects } from '../controllers/projectController.js';
+const router = express.Router();
+
+router.post('/', createProject);
+router.get('/', getAllProjects);
+
+export default router;
