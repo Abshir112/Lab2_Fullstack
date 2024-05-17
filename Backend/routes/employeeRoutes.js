f2bbc9d03d03eec4740af13ffd9259b7543fe523
abshir112 <abshir_muhumed.abdi0018@stud.hkr.se>
2024-05-18 00:26:29 +0200

    Add employee routes for creating and retrieving employees

---
+++ Backend/routes/employeeRoutes.js
@@ -0,0 +1,9 @@
+// backend/routes/employeeRoutes.js
+import express from 'express';
+import { createEmployee, getAllEmployees } from '../controllers/employeeController.js';
+const router = express.Router();
+
+router.post('/', createEmployee);
+router.get('/', getAllEmployees);
+
+export default router;
